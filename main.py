d0daba88735fbab189334e0e56fc5ee65c45c561
AntonyJR <A.Reynolds@computer.org>
2021-09-24 17:11:45 -0600

    Added support for nested json files using "import" special property in json objects.

--- main.py
+++ main.py
@@ -21,17 +21,21 @@ NANO_TO_MILLI = NANOS / MILLIS
 
 # Config File Properties
 ITERATIONS_FIELD = 'iterations'
+TARGETS_FIELD = 'targets'
+TARGET_NAME_FIELD = 'name'
 BASE_URL_FIELD = 'baseurl'
 BASIC_AUTH_SECRET_FIELD = 'secretOCID'
 JWT_AUTH_SECRET_FIELD = 'jwtSecretOCID'
 REQUESTS_FIELD = 'requests'
-NAME_FIELD = 'name'
+REQUEST_NAME_FIELD = 'name'
 PATH_FIELD = 'path'
 VERB_FIELD = 'verb'
 PARAMS_FIELD = 'params'
+PAYLOAD_FIELD = 'payload'
 ELAPSED_FIELDS_FIELD = 'elapsedFields'
 REPORT_NAME_FIELD = 'reportName'
 ELAPSED_PATH_FIELD = 'elapsedPath'
+INCLUDE_FIELD = 'include'
 
 # Secret Properties
 USERNAME_FIELD = 'username'
@@ -56,19 +60,19 @@ TOKEN_GRACE = 10
 
 class JWTAuth(AuthBase):
     def __init__(self, token_config):
-        '''
+        """
         Initialize authenticator
         :param token_config: Dictionary with properties for JWT assertion
-        '''
+        """
         self.token_config = token_config
         self.expiry = time() - TOKEN_GRACE
 
     def __call__(self, r):
-        '''
+        """
         Update request with authentication
         :param r: request
         :return: request with authentication header
-        '''
+        """
         now = time()
         if now > self.expiry:
             self.token_response = get_jwt_token(self.token_config)
@@ -79,15 +83,17 @@ class JWTAuth(AuthBase):
         r.headers[AUTH_HEADER] = self.token_response[TOKEN_TYPE_FIELD] + ' ' + self.token_response[ACCESS_TOKEN_FIELD]
         return r
 
-def print_error(output, err=False, verbose=False):
-    '''
+
+def print_error(output, err=False, verbose=False, end='\n'):
+    """
     Print to stderr, prefixed with current datetime
     :param output: Message to output
     :param err: Boolean to indicate if this is an error and so should always be output
     :param verbose: Boolean to indicate if verbose is set, meaning that message should be output
-    '''
+    :param end: Newline character
+    """
     if verbose or err:
-        print('{}: {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], output), file=stderr)
+        print('{}: {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], output), file=stderr, end=end)
 
 
 def init_oci():
@@ -113,7 +119,6 @@ def get_oci_secret(oci_config, oic_secret_ocid):
     """
     sc = SecretsClient(oci_config)
     response = sc.get_secret_bundle(oic_secret_ocid)
-    print_error('Retrieved Secret', verbose=args.verbose)
     base64_secret_content = response.data.secret_bundle_content.content
     base64_secret_bytes = base64_secret_content.encode(AUTH_ENCODING)
     base64_message_bytes = b64decode(base64_secret_bytes)
@@ -122,7 +127,7 @@ def get_oci_secret(oci_config, oic_secret_ocid):
 
 
 def get_jwt_assertion(kid, sub, lifetime, iss, private_key):
-    '''
+    """
     Create a JWT assertion
     :param kid: Key identifier
     :param sub: Subject
@@ -130,7 +135,7 @@ def get_jwt_assertion(kid, sub, lifetime, iss, private_key):
     :param iss: IDCS client id
     :param private_key: Private key relating to kid
     :return: JWT Assertion
-    '''
+    """
     jwt_headers = {
         # 'kid': 'OicClientCredentialsOauth'
         'kid': kid
@@ -148,11 +153,11 @@ def get_jwt_assertion(kid, sub, lifetime, iss, private_key):
 
 
 def get_jwt_token(token_config):
-    '''
+    """
     Obtain a token from OAuth server
     :param token_config: configuration of token
     :return: JWT token response
-    '''
+    """
     token_request = {
         'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
         'assertion': get_jwt_assertion(kid=token_config[KID_FIELD], sub=token_config[SUB_FIELD],
@@ -165,10 +170,10 @@ def get_jwt_token(token_config):
 
 
 def init_parser():
-    '''
+    """
     INitialize command line parser
     :return: Initialized parser
-    '''
+    """
     parser = ArgumentParser(description='Performance Test OIC Integrations')
     parser.add_argument('configFile', help='Configuration File')
     parser.add_argument('--noheader', action='store_true', help='Do not output a header for the results table')
@@ -176,56 +181,89 @@ def init_parser():
     return parser.parse_args()
 
 
+def init_config(filename):
+    """
+    Initialize configuration from config file
+    :param filename:
+    :return: config
+    """
+    # Load File
+    with open(filename, 'r') as cf:
+        root_config = load(cf)
+
+    # Find any includes
+    includes_expression = parse('$..' + INCLUDE_FIELD)
+    selection = includes_expression.find(root_config)
+
+    # Substitute includes
+    for matched in selection:
+        replacement = init_config(matched.value)
+        matched.context.path.update(root_config, replacement)
+
+    return root_config
+
+
 if __name__ == '__main__':
     # Get configuration
     args = init_parser()
-    with open(args.configFile, 'r') as cf:
-        config = load(cf)
-    print_error('Loaded config', verbose=args.verbose)
+    config = init_config(args.configFile)
 
     # Initialize OCI config
     oci_config = init_oci()
-    print_error('Initialized OCI', verbose=args.verbose)
-
-    # Initialize JWT or Basic Auth based on presence of JWT_AUTH_SECRET_FIELD
-    if JWT_AUTH_SECRET_FIELD in config:
-        secret = get_oci_secret(oci_config, config[JWT_AUTH_SECRET_FIELD])
-        auth = JWTAuth(secret)
-        auth_method = 'JWT'
-    else:
-        oic_credentials = get_oci_secret(oci_config, config[BASIC_AUTH_SECRET_FIELD])
-        auth = (oic_credentials[USERNAME_FIELD], oic_credentials[PASSWORD_FIELD])
-        auth_method = 'Basic'
-    print_error('Created authorization', verbose=args.verbose)
 
     tests = []
-    print_error('Starting {} iterations'.format(config[ITERATIONS_FIELD]), verbose=args.verbose)
-    # Run test ITERATIONS_FIELD times
-    for cnt in range(config[ITERATIONS_FIELD]):
-        # Iterate over request list in config
-        for req in config[REQUESTS_FIELD]:
-            entry = {'Auth': auth_method, NAME_FIELD: req[NAME_FIELD]}
-            url = config[BASE_URL_FIELD] + req[PATH_FIELD]
-            start = time()
-            if req[VERB_FIELD] == 'GET':
-                resp = get(config[BASE_URL_FIELD] + req[PATH_FIELD], auth=auth, params=req[PARAMS_FIELD])
-            elif req[VERB_FIELD] == 'POST':
-                resp = post(config[BASE_URL_FIELD] + req[PATH_FIELD], auth=auth, data=req[PARAMS_FIELD])
-            else:
-                resp = None
-            elapsed = time() - start
-            entry['elapsed'] = elapsed
-            resp_json = resp.json()
-            result = [{'elapsed', elapsed}]
-            for field in req[ELAPSED_FIELDS_FIELD]:
-                jsonpath = parse(field[ELAPSED_PATH_FIELD])
-                result = jsonpath.find(resp_json)
-                if len(result) > 0:
-                    entry[field[REPORT_NAME_FIELD]] = int(float(result[0].value))
+    # Iterate over targets
+    for target in config[TARGETS_FIELD]:
+        # Initialize JWT or Basic Auth based on presence of JWT_AUTH_SECRET_FIELD
+        if JWT_AUTH_SECRET_FIELD in target:
+            secret = get_oci_secret(oci_config, target[JWT_AUTH_SECRET_FIELD])
+            auth = JWTAuth(secret)
+            auth_method = 'JWT'
+        else:
+            oic_credentials = get_oci_secret(oci_config, target[BASIC_AUTH_SECRET_FIELD])
+            auth = (oic_credentials[USERNAME_FIELD], oic_credentials[PASSWORD_FIELD])
+            auth_method = 'Basic'
+
+        # Run test ITERATIONS_FIELD times
+        print_error('Starting {} iterations on {}'.format(config[ITERATIONS_FIELD], target[BASE_URL_FIELD]),
+                    verbose=args.verbose)
+        for cnt in range(config[ITERATIONS_FIELD]):
+
+            # Iterate over request list in config
+            for req in config[REQUESTS_FIELD]:
+                entry = {'Target': target[TARGET_NAME_FIELD], 'Auth': auth_method,
+                         REQUEST_NAME_FIELD: req[REQUEST_NAME_FIELD]}
+                url = target[BASE_URL_FIELD] + req[PATH_FIELD]
+                params = req.get(PARAMS_FIELD, None)
+                payload = req.get(PAYLOAD_FIELD, None)
+                start = time()
+                if req[VERB_FIELD] == 'GET':
+                    resp = get(url, auth=auth, params=params)
+                elif req[VERB_FIELD] == 'POST':
+                    resp = post(url, auth=auth, params=params, json=payload)
                 else:
-                    entry[field[REPORT_NAME_FIELD]] = 'N/A'
-                    print_error('Missing response field {}: {}'.format(field[ELAPSED_PATH_FIELD], resp), err=True)
-            tests.append(entry)
+                    resp = None
+                elapsed = time() - start
+                entry['elapsed'] = int(elapsed * 1000)
+                entry['status'] = resp.status_code
+                if resp.ok:
+                    resp_json = resp.json()
+                    for field in req[ELAPSED_FIELDS_FIELD]:
+                        jsonpath = parse(field[ELAPSED_PATH_FIELD])
+                        result = jsonpath.find(resp_json)
+                        if len(result) > 0:
+                            entry[field[REPORT_NAME_FIELD]] = int(float(result[0].value))
+                        else:
+                            entry[field[REPORT_NAME_FIELD]] = 'N/A'
+                            print_error('Missing response field {}: {}'.format(field[ELAPSED_PATH_FIELD], resp),
+                                        err=True)
+                    print('😀', file=stderr, end='', flush=True)
+                else:
+                    print_error('Error {} returned {}'.format(url, resp.status_code), err=True)
+                    for field in req[ELAPSED_FIELDS_FIELD]:
+                        entry[field[REPORT_NAME_FIELD]] = 'N/A'
+                    print('😞', file=stderr, end='', flush=True)
+                tests.append(entry)
 
     # Write results in CSV format
     writer = DictWriter(stdout, list(tests[0].keys()))
